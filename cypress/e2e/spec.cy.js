34fc3fd3c1c8dbab55759dc509b200f3c0ed9e07
Patrick Venturini <pmventurini98@gmail.com>
2026-03-29 16:02:39 -0300

    testes e2e cypress - testando fluxo de cadastro

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.contains('Cadastro', { timeout: 10000 });
+    cy.get('input[aria-label="Nome"]').type('Patrick')
+    cy.get('input[aria-label="Email"]').type('email@teste.com')
+
+    cy.contains('Cadastrar').click({ force: true })
+
+    cy.contains('Patrick')
+      .should('be.visible')
+      .and('contain.text', 'email@teste.com');
+  });
+})
